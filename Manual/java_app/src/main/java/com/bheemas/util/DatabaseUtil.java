2e7663a93f8afa4346655b35a772f944e9703001
Bheem Medi <eeswar887@gmail.com>
2025-09-14 20:25:59 +0530

    Create DatabaseUtil.java

---
+++ Manual/java_app/src/main/java/com/bheemas/util/DatabaseUtil.java
@@ -0,0 +1,36 @@
+package com.bheemas.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class DatabaseUtil {
+    private static Connection connection = null;
+    
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        } else {
+            try {
+                Properties prop = new Properties();
+                InputStream inputStream = DatabaseUtil.class.getClassLoader()
+                        .getResourceAsStream("database.properties");
+                prop.load(inputStream);
+                
+                String driver = prop.getProperty("driver");
+                String url = prop.getProperty("url");
+                String user = prop.getProperty("user");
+                String password = prop.getProperty("password");
+                
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException | SQLException | IOException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+    }
+}
