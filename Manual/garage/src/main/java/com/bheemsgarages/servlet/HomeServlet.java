364d70b761a75b745b4bd5ce221c3a60f5661d70
Bheem Medi <eeswar887@gmail.com>
2025-09-28 21:02:58 +0530

    Update HomeServlet.java

--- Manual/garage/src/main/java/com/bheemsgarages/servlet/HomeServlet.java
+++ Manual/garage/src/main/java/com/bheemsgarages/servlet/HomeServlet.java
@@ -1,1 +1,16 @@
 
+package com.bheemsgarages.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
+    }
+}
